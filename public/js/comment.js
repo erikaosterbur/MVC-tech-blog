5fd70f5318a86fcc56a4402d1ca4180fc3862909
erikaosterbur <erikaosterbur@gmail.com>
2021-11-03 15:59:47 -0500

    fixed put route and comment bugs

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,16 +1,14 @@
+const post_id = document.querySelector('input[name="post-id"]').value;
+
 const commentFormHandler = async (event) => {
     event.preventDefault();
   
-    
     const body = document.querySelector('#comment-body').value.trim();
-    const postId = document.querySelector('#comment-post-id').textContent;
-   
-    console.log(body, postId);
   
     if (body) {
       const response = await fetch('/api/comments', {
         method: 'POST',
-        body: JSON.stringify({ body, postId }),
+        body: JSON.stringify({ body, post_id }),
         headers: { 'Content-Type': 'application/json' },
       });
   
--- public/js/edit.js
+++ public/js/edit.js
@@ -16,14 +16,14 @@ const delButtonHandler = async (event) => {
 const updateButtonHandler = async (event) => {
     event.preventDefault();
 
-    const post_id = document.querySelector('#edit-post-id').textContent;
-    const post_title = document.querySelector('#edited-title').value.trim();
-    const post_body = document.querySelector('#edited-body').value.trim();
+    const post_id = document.querySelector('#edit-post-id').value;
+    const title = document.querySelector('#edited-title').value.trim();
+    const body = document.querySelector('#edited-body').value.trim();
 
-    if (post_title && post_body) {
+    if (title && body) {
         const response = await fetch(`/api/posts/${post_id}`, {
             method: 'PUT',
-            body: JSON.stringify({post_title, post_body}),
+            body: JSON.stringify({title, body}),
             headers: {
                 'Content-Type': 'application/json',
             },
